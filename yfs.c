7370ec6d380663ef5817365cdde4a5d11b7f663f
Alex Berlaga <awb4@rice.edu>
2022-04-14 18:22:15 -0500

    message sending

--- yfs.c
+++ yfs.c
@@ -6,60 +6,109 @@
 #include <comp421/iolib.h>
 #include <comp421/hardware.h>
 
+#define IPB BLOCKSIZE / INODESIZE
 
-#define DEFAULT_NUM_BLOCKS 128
-#define DEFAULT_NUM_INODES 64
+bool *block_bitmap;
+bool *inode_bitmap;
+
+
+
+union {
+    struct fs_header head;
+    struct inode inode[IPB];
+    char buf[BLOCKSIZE];
+} *read_block;
 
 struct message {
     int type;
+    pid_t pid;
     char text[24];
 };
 
 struct message *message;
 struct fs_header header;
 
-int InitFileSystem(int num_blocks, int num_inodes);
-void *entry(int num_blocks, int offset);
+
 
 
 int 
 main(char *argc, char **argv) 
 {
+    if (Register(FILE_SERVER) == -1) {
+        TracePrintf(0, "Register File Server: Freak the Fuck out\n");
+        Exit(-1);
+    }
     if (Fork() == 0) {
         Exec(argv[1], argv + 1);
     } else {
-        int nb = DEFAULT_NUM_BLOCKS;
-        int ni = DEFAULT_NUM_INODES;
-        if (argc >= 3) {
-            nb = (int) argv[1];
-            ni = (int) argv[2];
+        if (ReadSector(1, read_block) < 0) {
+            TracePrintf(0, "ReadSector: Freak the Fuck out\n");
+            Exit(-1);
         }
-        if (InitFileSystem(nb, ni) == -1) {
-            TracePrintf(0, "YFS Main: Freak the Fuck out");
-            return -1;
+        int nb = read_block->head.num_blocks;
+        int ni = read_block->head.num_inodes;
+        int maxfb =  nb - ceil((ni + 1) / IPB); 
+        block_bitmap = malloc(sizeof(bool) * maxfb);
+        inode_bitmap = malloc(sizeof(bool) * ni);
+        
+        int i;
+        //Init free blocks
+        for (i = 1; i < maxfb; i++) {
+            block_bitmap[i] = true;
         }
+        
+        block_bitmap[0] = false;
+
+        
+        //Init free inodes
+        for (i = 2; i < ni; i++) {
+            inode_bitmap[i] = true;
+        }
+
+        inode_bitmap[0] = false;
+        inode_bitmap[1] = false;
+       
         while(1) {
-            pid_t pid = Receive(*message);
+            
+            pid_t pid = Receive(message);
+            switch (message->type) {
+                case 0:
+                    //text[0:8] = old name
+                    //text[8:16] = new name
+                    char *oldname = GetPathName(message, 0);
+                    char *newname = GetPathName(message, 8);
+                    Link(oldname, newname);
+                    break;  
+                case 1:
+                    //text[0:8] = name
+                    Unlink(GetPathName(message, 0));
+                    break;
+                case 2: 
+                    SymLink(NULL, NULL);
+                    break;
+                case 3:
+                    //text[0:8] = pathname
+                    
+                    char *pathname = GetPathName(message, 0);
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+            }
             /* Construct and overwrite the message to reply */
             Reply(*message, pid);
         }
     }
 }
 
-int 
-InitFileSystem(int num_blocks, int num_inodes)
-{
-    root = malloc(sizeof(struct file_tree));
-    header.num_blocks = num_blocks;
-    header.num_inodes = num_inodes;
-    filesystem = malloc(BLOCKSIZE * num_blocks);
-    int i;
-    for (i = 0; i < ceil((num_inodes + 1) * INODESIZE / BLOCKSIZE) ; i++) {
-        void 
-    }
-    /* Initialize fields of the root */
-    cd = root;
-}
 
 void *
 entry(int block_num, int inode_offset) {
@@ -67,3 +116,11 @@ entry(int block_num, int inode_offset) {
     return file + block_num * BLOCKSIZE + inode_offset * INODESIZE;
 }
 
+char *
+GetPathName(struct message *msg, int text_pos) {
+    char *name = malloc(MAXPATHNAMELEN);
+                    
+    char *src = (char *) message->text[text_pos];
+
+    CopyFrom(message->pid, name, src, MAXPATHNAMELEN);
+}
