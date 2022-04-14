894d452ae9cc40b76a573fe95467a2fbf2a21546
Alex Berlaga <awb4@rice.edu>
2022-04-14 16:52:36 -0500

    init

--- yfs.c
+++ yfs.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 #include <comp421/iolib.h>
 #include <comp421/hardware.h>
+
+
+#define DEFAULT_NUM_BLOCKS 128
+#define DEFAULT_NUM_INODES 64
 
 struct message {
     int type;
@@ -13,12 +18,26 @@ struct message {
 struct message *message;
 struct fs_header header;
 
+int InitFileSystem(int num_blocks, int num_inodes);
+void *entry(int num_blocks, int offset);
+
+
 int 
 main(char *argc, char **argv) 
 {
     if (Fork() == 0) {
         Exec(argv[1], argv + 1);
     } else {
+        int nb = DEFAULT_NUM_BLOCKS;
+        int ni = DEFAULT_NUM_INODES;
+        if (argc >= 3) {
+            nb = (int) argv[1];
+            ni = (int) argv[2];
+        }
+        if (InitFileSystem(nb, ni) == -1) {
+            TracePrintf(0, "YFS Main: Freak the Fuck out");
+            return -1;
+        }
         while(1) {
             pid_t pid = Receive(*message);
             /* Construct and overwrite the message to reply */
@@ -26,3 +45,25 @@ main(char *argc, char **argv)
         }
     }
 }
+
+int 
+InitFileSystem(int num_blocks, int num_inodes)
+{
+    root = malloc(sizeof(struct file_tree));
+    header.num_blocks = num_blocks;
+    header.num_inodes = num_inodes;
+    filesystem = malloc(BLOCKSIZE * num_blocks);
+    int i;
+    for (i = 0; i < ceil((num_inodes + 1) * INODESIZE / BLOCKSIZE) ; i++) {
+        void 
+    }
+    /* Initialize fields of the root */
+    cd = root;
+}
+
+void *
+entry(int block_num, int inode_offset) {
+    char *file = (char *) filesystem;
+    return file + block_num * BLOCKSIZE + inode_offset * INODESIZE;
+}
+
